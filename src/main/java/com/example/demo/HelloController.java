98963d8d5d4945f02f65d4d76dbe585bc484dc56
jakovistuk <jistuk@gmail.com>
2020-05-22 14:21:57 +0200

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,12 +3,20 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+import java.util.Properties;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        StringBuffer sb = new StringBuffer();
+        sb.append("Greetings from Spring Boot!\n\n");
+        Map<String, String> enviorntmentVars  = System.getenv();
+        enviorntmentVars.entrySet().forEach(sb::append);
+        
+        return sb.toString();
     }
 
 }
